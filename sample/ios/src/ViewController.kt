bb4719aff64f9b98035d32d7ecb87af9595e89fe
sumy <sumyggsun@gmail.com>
2025-07-15 01:43:54 +0800

    init kansha-rs

--- sample/ios/src/ViewController.kt
+++
@@ -1,7 +0,0 @@
-import androidx.compose.ui.window.ComposeUIViewController
-import androidx.compose.foundation.text.BasicText
-
-fun ViewController() = ComposeUIViewController {
-    ComposeApp()
-//    BasicText("Testing..")
-}
---
+++ tests/example_test.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test_add() {
+    let result = 2 + 2;
+    assert_eq!(result, 4, "2 + 2 should equal 4");
+}
